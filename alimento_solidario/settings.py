542276832a29d545c83d0aabae39fcd782c7a68e
fernando-izar <fizar@me.com>
2022-12-28 08:49:13 -0300

    feat: add allowed host to settings.py

--- alimento_solidario/settings.py
+++ alimento_solidario/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ec2-54-232-73-207.sa-east-1.compute.amazonaws.com"]
 
 
 # Application definition
