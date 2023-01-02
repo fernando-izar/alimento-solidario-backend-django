59793b5d5ab05f9f0d8550cac33599556bc794ea
fernando-izar <fizar@me.com>
2023-01-02 08:34:59 -0300

    feat: routes classifications/ and classifications/<str:pk> ok

--- classifications/views.py
+++ classifications/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from rest_framework import generics, status
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from users.permissions import IsAdm, IsOwnerOrAdm
+from .serializers import ClassificationSerializer
+from .models import Classification
 
 
 class ClassificationView(generics.ListCreateAPIView):
-    ...
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsAdm]
+
+    queryset = Classification.objects.all()
+    serializer_class = ClassificationSerializer
 
 
 class ClassificationDetailView(generics.RetrieveUpdateDestroyAPIView):
-    ...
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsAdm]
+
+    queryset = Classification.objects.all()
+    serializer_class = ClassificationSerializer
 
 
 class ClassificationNameView(generics.ListAPIView):
