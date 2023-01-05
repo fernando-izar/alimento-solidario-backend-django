42afe09550c6bbb208c3f976fc8dd13923cc006e
fernando-izar <fizar@me.com>
2023-01-05 08:25:07 -0300

    feat: test login is not sucessful with invalid email and password

--- users/tests.py
+++ users/tests.py
@@ -305,7 +305,19 @@ class UserRegisterViewTest(APITestCase):
         resulted_status_code = response.status_code
         msg = "Verify if the status code is 401 when trying to login with invalid password"
         self.assertEqual(expected_status_code, resulted_status_code, msg=msg)
-        
-    
 
+    
+    # Verify if login is not successful with invalid email and password
+    def test_login_is_not_successful_with_invalid_email_and_password(self):
+        user = baker.make(User, type="donor", email="test@mail.com", password="123456")
+        user.set_password(user.password)
+        user.save()
+
+        url = reverse("login")
+        data = {"email": "invalid_email@mail.com", "password": "invalid_password"}
+        response = self.client.post(url, data)
+        expected_status_code = status.HTTP_401_UNAUTHORIZED
+        resulted_status_code = response.status_code 
+        msg = "Verify if the status code is 401 when trying to login with invalid email and password"
+        self.assertEqual(expected_status_code, resulted_status_code, msg=msg)
 
