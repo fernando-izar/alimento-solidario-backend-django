ebb06a11fd5213c8a1fe207c4bec108fb65a8013
João Dantas Gomes de Araujo <103146770+JoaoDantaas@users.noreply.github.com>
2023-01-06 12:50:57 -0300

    Revert "As 4"

--- classifications/urls.py
+++ classifications/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("classifications/", views.ClassificationView.as_view(), name="classifications"),
     path("classifications/name/<str:name>/", views.ClassificationNameView.as_view()),
+    path("classifications/", views.ClassificationView.as_view()),
     path("classifications/<str:pk>/", views.ClassificationDetailView.as_view()),
 ]
